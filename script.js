a456d8df55ca42908edb94002b288f725ef084c1
Abhay Patil <patilabhay484@gmail.com>
2025-02-16 13:11:39 +0530

    Home Page completed

--- script.js
+++ script.js
@@ -16,4 +16,70 @@ document.addEventListener("DOMContentLoaded" , function(){
         count ++ ;
     }
     viewfeatureBtn.addEventListener( "click" , afterviewfeatureBtnClick );
+
+
+    let leftArrowBtn = document.querySelector(".leftArrow");
+    let rightArrowBtn = document.querySelector(".rightArrow");
+
+    let divArray = document.querySelectorAll(".role");
+    let totalItems = divArray.length;
+    let visibleItems ; 
+
+    let leftCount = 0;
+    let rightCount = totalItems - 1;
+
+    let screenWidth = window.screen.width;
+    screenWidth = Number(screenWidth);
+    console.log(screenWidth);
+    if( screenWidth < 700 && screenWidth > 500 ){
+        console.log(screenWidth)
+        visibleItems = 3 ;
+    }
+    else if( screenWidth < 500 ){
+        console.log(screenWidth)
+        visibleItems = 2 ;
+    }
+    else{
+        console.log(screenWidth)
+        visibleItems = 4 ;
+    }
+
+    for (let i = 0; i < visibleItems; i++) {
+        if (divArray[i]) {
+            divArray[i].style.display = "flex";
+        }
+    }
+
+    function hideAll() {
+        divArray.forEach(div => div.style.display = "none");
+    }
+
+    function leftArrowFunction() {
+        if (leftCount + visibleItems < totalItems) {
+            leftCount++;
+            hideAll();
+            for (let i = leftCount; i < leftCount + visibleItems; i++) {
+                if (divArray[i]) {
+                    divArray[i].style.display = "flex";
+                }
+            }
+        }
+    }
+
+    function rightArrowFunction() {
+        if (leftCount > 0) {
+            leftCount--;
+            hideAll();
+            for (let i = leftCount; i < leftCount + visibleItems; i++) {
+                if (divArray[i]) {
+                    divArray[i].style.display = "flex";
+                }
+            }
+        }
+    }
+
+    leftArrowBtn.addEventListener("click", leftArrowFunction);
+    rightArrowBtn.addEventListener("click", rightArrowFunction);
+
+    
 })
